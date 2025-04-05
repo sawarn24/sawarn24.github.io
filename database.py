49080e2c2a610c8b5a2963714fc4732fc81c801d
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2025-04-06 02:58:26 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,23 @@
+import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from config import DATABASE_URL
 
 def get_db_connection():
     try:
-        conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
+        
+        if os.environ.get("DATABASE_URL", "").startswith("postgres://"):
+            
+            database_url = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://")
+            conn = psycopg2.connect(database_url, sslmode='require')
+        else:
+            
+            conn = psycopg2.connect(DATABASE_URL)
+        
+        conn.autocommit = False
+        conn.cursor_factory = RealDictCursor
         print("Database connected successfully!")
         return conn
     except Exception as e:
-        print("Error connecting to database:", e)
+        print(f"Database connection error: {e}")
         raise e
