e9313dbb809671386faf9857cbe0a47c0cd12529
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2025-04-06 03:00:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-DATABASE_URL="dbname=inventory_db user=postgres password=sawarn@2411 host=localhost port=5432"
-SECRET_KEY = "787045"  # Change this to a strong, unique key
+import os
+
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "dbname=inventory_db user=postgres password=sawarn@2411 host=localhost port=5432")
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "787045")  
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
